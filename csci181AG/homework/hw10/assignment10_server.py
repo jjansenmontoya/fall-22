395fdfd8d2a0ad5261232b4347f8679ebb33e025
jjansenmontoya <jjansenmontoya@g.hmc.edu>
2022-12-05 12:52:22 -0800

    initial work

---
+++ csci181AG/homework/hw10/assignment10_server.py
@@ -0,0 +1,14 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 65432
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((HOST, PORT))
+s.listen()
+data = ""
+while True: 
+    conn, addr = s.accept()
+    conn.sendall()
+s.close()
