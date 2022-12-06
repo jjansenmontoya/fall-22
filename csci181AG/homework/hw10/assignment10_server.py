6ee029322f9cc6ec95dcb3ab482096f14ec3545e
jjansenmontoya <jjansenmontoya@g.hmc.edu>
2022-12-05 19:30:40 -0800

    finishing work

--- csci181AG/homework/hw10/assignment10_server.py
+++ csci181AG/homework/hw10/assignment10_server.py
@@ -7,8 +7,20 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.bind((HOST, PORT))
 s.listen()
-data = ""
 while True: 
     conn, addr = s.accept()
-    conn.sendall()
+    recievedMessage = conn.recv(4096).decode()
+    try:
+        requestedFile = recievedMessage[5:15]
+        file = open(requestedFile,"r")
+        contents = file.read()
+        file.close()
+        initialLine = "HTTP/1.1 200 OK\n"
+        secondLines = "Content-Type:text/html\nConnection:close\n\n"
+        message = initialLine + secondLines + contents
+    except:
+        initialLine = "HTTP/1.1 404 Not Found\n"
+        secondLines = "Content-Type:text/html\nConnection:close\n\n"
+        message = initialLine + secondLines + "failure"
+    conn.sendall(message.encode())
 s.close()
